61d019a8c2eaad52d29481371cf7f3c2b2797041
Alberto Molina Arribere <amolina@vaudoise.ch>
2022-02-03 14:55:02 +0100

    NFTMarket tests

--- test/Tests_NFTMarket.js
+++ test/Tests_NFTMarket.js
@@ -5,6 +5,8 @@ const init = require("../test_libraries/InitializeContracts.js");
 
 const NFTMarket = artifacts.require("NFTMarket");
 const NFTMarketAbi = NFTMarket.abi;
+const PublicPool = artifacts.require("PublicPool");
+const PublicPoolAbi = PublicPool.abi;
 
 const Gas = constants.Gas;
 const NewIssuerFee = constants.NewIssuerFee;
@@ -15,6 +17,7 @@ const TransferFeeAmount = constants.TransferFeeAmount;
 const TransferFeeDecimals = constants.TransferFeeDecimals;
 const AdminTransferFeeAmount = constants.AdminTransferFeeAmount;
 const AdminTransferFeeDecimals = constants.AdminTransferFeeDecimals;
+const mintingFee = MintingFee.plus(AdminMintingFee)
 
 
 
@@ -24,6 +27,10 @@ const AdminTransferFeeDecimals = constants.AdminTransferFeeDecimals;
 contract("Testing Public Pool",function(accounts){
     var manager;
     var publicpoolProxy;
+    var Market_1;
+    var Market_2;
+
+    const OnlyOwner = RegExp("Only the Owner is allowed to do that");
 
     // used addresses
     const chairPerson = accounts[0];
@@ -56,7 +63,8 @@ contract("Testing Public Pool",function(accounts){
     const NotEnoughFees = new RegExp("New Issuer Fees not enough");
     const TooMuch = new RegExp("EC20-");
     const IssuerIDTaken = new RegExp("This Issuer Name has already been taken");
-
+    const MintingFeesNotEnough = new RegExp("Minting Fees not enough");
+    const OnlyOwnerOrApproved = new RegExp("Only owner or approved can change token price");
 
 
     beforeEach(async function(){
@@ -65,60 +73,159 @@ contract("Testing Public Pool",function(accounts){
         publicpoolProxy = new web3.eth.Contract(PublicPoolAbi, contracts[1][0]);
     });
 
-    function GenerateMarkets(){
+    async function GenerateMarkets(secondMarket){
         let response_1 = await publicpoolProxy.methods.requestIssuer(issuer_1, issuer_1_name, issuer_1_symbol, issuer_1_fee, issuer_1_decimals, issuer_1_paymentplans).send({from: user_1, gas: Gas, value: NewIssuerAmount}, function(error, result){});
-        let response_2 = await publicpoolProxy.methods.requestIssuer(issuer_2, issuer_2_name, issuer_2_symbol, issuer_2_fee, issuer_2_decimals, issuer_2_paymentplans).send({from: user_1, gas: Gas, value: NewIssuerAmount}, function(error, result){});
         let issuerId_1 = new BigNumber(response_1.events._NewIssuerRequest.returnValues.id);
-        let issuerId_2 = new BigNumber(response_2.events._NewIssuerRequest.returnValues.id);
+        await publicpoolProxy.methods.validateIssuer(issuerId_1).send({from: PublicOwners[0], gas: Gas}, function(error, result){});
+        await publicpoolProxy.methods.validateIssuer(issuerId_1).send({from: PublicOwners[1], gas: Gas}, function(error, result){});
+        let Issuers_1_Address = await publicpoolProxy.methods.retrieveNFTMarketForIssuer(issuerId_1).call();
+        Market_1 = new web3.eth.Contract(NFTMarketAbi, Issuers_1_Address);
 
-        await publicpoolProxy.methods.validateIssuer(issuerId).send({from: PublicOwners[0], gas: Gas}, function(error, result){});
-
+        if(true == secondMarket){
+            let response_2 = await publicpoolProxy.methods.requestIssuer(issuer_2, issuer_2_name, issuer_2_symbol, issuer_2_fee, issuer_2_decimals, issuer_2_paymentplans).send({from: user_1, gas: Gas, value: NewIssuerAmount}, function(error, result){});
+            let issuerId_2 = new BigNumber(response_2.events._NewIssuerRequest.returnValues.id);
+            await publicpoolProxy.methods.validateIssuer(issuerId_2).send({from: PublicOwners[0], gas: Gas}, function(error, result){});
+            await publicpoolProxy.methods.validateIssuer(issuerId_2).send({from: PublicOwners[1], gas: Gas}, function(error, result){});
+            let Issuers_2_Address = await publicpoolProxy.methods.retrieveNFTMarketForIssuer(issuerId_2).call();
+            Market_2 = new web3.eth.Contract(NFTMarketAbi, Issuers_2_Address);
+        }
     }
 
 
-    // ****** TESTING New Issuers ***************************************************************** //
+    // ****** TESTING Chaging Configs ***************************************************************** //
 
     it("Change Configuration WRONG",async function(){
+        await GenerateMarkets(false);
         try{
-            await publicpoolProxy.methods.requestIssuer(issuer_1, issuer_1_name, issuer_1_symbol, 1001, 1, 0).send({from: user_1, gas: Gas, value: NewIssuerAmount}, function(error, result){});
+            await Market_1.methods.changeOwner(user_1).send({from: user_1, gas: Gas}, function(error, result){});
+            expect.fail();
+        }
+        // assert
+        catch(error){
+            expect(error.message).to.match(OnlyOwner);
+        }
+        try{
+            await Market_1.methods.changePaymentPlan(issuer_2_paymentplans).send({from: user_1, gas: Gas}, function(error, result){});
+            expect.fail();
+        }
+        // assert
+        catch(error){
+            expect(error.message).to.match(OnlyOwner);
+        }
+        try{
+            await Market_1.methods.changeOffersLifeTime(1).send({from: user_1, gas: Gas}, function(error, result){});
+            expect.fail();
+        }
+        // assert
+        catch(error){
+            expect(error.message).to.match(OnlyOwner);
+        }
+        try{
+            await Market_1.methods.changeOwnerTransferFees(issuer_2_fee, issuer_2_decimals).send({from: user_1, gas: Gas}, function(error, result){});
+            expect.fail();
+        }
+        // assert
+        catch(error){
+            expect(error.message).to.match(OnlyOwner);
+        }
+        try{
+            await Market_1.methods.changeOwnerTransferFees(10001, 2).send({from: issuer_1, gas: Gas}, function(error, result){});
             expect.fail();
         }
         // assert
         catch(error){
             expect(error.message).to.match(FeesNOK);
         }
-        // act
+       
+    });
+
+    it("Change Configuration CORRECT",async function(){
+        await GenerateMarkets(false);
+        let newLifeTime = 1;
+        let before = await Market_1.methods.retrieveIssuer().call();
+        await Market_1.methods.changePaymentPlan(issuer_2_paymentplans).send({from: issuer_1, gas: Gas}, function(error, result){});
+        await Market_1.methods.changeOffersLifeTime(newLifeTime).send({from: issuer_1, gas: Gas}, function(error, result){});
+        await Market_1.methods.changeOwner(user_1).send({from: issuer_1, gas: Gas}, function(error, result){});
+        await Market_1.methods.changeOwnerTransferFees(issuer_2_fee, issuer_2_decimals).send({from: user_1, gas: Gas}, function(error, result){});
+        let after = await Market_1.methods.retrieveIssuer().call();
+
+        expect(before[0]._owner).to.equal(issuer_1.toString());
+        expect(after[0]._owner).to.equal(user_1.toString());
+        expect(before[0]._paymentPlan).to.equal(issuer_1_paymentplans.toString());
+        expect(after[0]._paymentPlan).to.equal(issuer_2_paymentplans.toString());
+        expect(before[0]._feeAmount).to.equal(issuer_1_fee.toString());
+        expect(after[0]._feeAmount).to.equal(issuer_2_fee.toString());
+        expect(before[0]._feeDecimals).to.equal(issuer_1_decimals.toString());
+        expect(after[0]._feeDecimals).to.equal(issuer_2_decimals.toString());
+        expect(after[1]).to.equal(newLifeTime.toString());
+       
+    });
+
+     // ****** TESTING Minting ***************************************************************** //
+     it("Minting WRONG",async function(){
+        await GenerateMarkets(false);
         try{
-            await publicpoolProxy.methods.requestIssuer(address_0, issuer_1_name, issuer_1_symbol, 10, 0, 0).send({from: user_1, gas: Gas, value: NewIssuerAmount}, function(error, result){});
+            await Market_1.methods.mintToken(0, user_1, 10).send({from: user_1, gas: Gas, value: mintingFee}, function(error, result){});
             expect.fail();
         }
         // assert
         catch(error){
-            expect(error.message).to.match(AddressNOK);
+            expect(error.message).to.match(OnlyOwner);
         }
-        // act
         try{
-            let amount = NewIssuerAmount.minus(1)
-            await publicpoolProxy.methods.requestIssuer(issuer_1, issuer_1_name, issuer_1_symbol, 10, 0, 0).send({from: user_1, gas: Gas, value: amount}, function(error, result){});
+            await Market_1.methods.mintToken(0, user_1, 10).send({from: issuer_1, gas: Gas, value: mintingFee.minus(1)}, function(error, result){});
             expect.fail();
         }
         // assert
         catch(error){
-            expect(error.message).to.match(NotEnoughFees);
-        }
-        // act
-        try{
-            await publicpoolProxy.methods.requestIssuer(issuer_1, issuer_1_name, issuer_1_symbol, 10, 0, 0).send({from: user_1, gas: Gas, value: NewIssuerAmount}, function(error, result){});
-            await publicpoolProxy.methods.requestIssuer(issuer_2, issuer_1_name, "", 10, 0, 0).send({from: user_1, gas: Gas, value: NewIssuerAmount}, function(error, result){});
-            expect.fail();
-        }
-        // assert
-        catch(error){
-            expect(error.message).to.match(IssuerIDTaken);
+            expect(error.message).to.match(MintingFeesNotEnough);
         }
     });
 
-  
+    it("Minting CORRECT",async function(){
+        await GenerateMarkets(true);
+        let tokenId = 0;
+        let tokenPrice = 10;
+        await Market_1.methods.mintToken(tokenId, user_1, tokenPrice).send({from: issuer_1, gas: Gas, value: mintingFee}, function(error, result){});
+        let token_1 = await Market_1.methods.retrieveToken(tokenId).call();
+        await Market_2.methods.mintToken(tokenId, user_1, tokenPrice).send({from: issuer_2, gas: Gas}, function(error, result){});
+        let token_2 = await Market_2.methods.retrieveToken(tokenId).call();
+
+        expect(token_1[0]._paymentPlan).to.equal(issuer_1_paymentplans.toString());
+        expect(token_1[0]._price).to.equal(tokenPrice.toString());
+        expect(token_1[1]).to.equal(user_1.toString());
+        expect(token_2[0]._paymentPlan).to.equal(issuer_2_paymentplans.toString());
+        expect(token_2[0]._price).to.equal(tokenPrice.toString());
+        expect(token_2[1]).to.equal(user_1.toString());
+    });
+
+    // ****** TESTING Token Price ***************************************************************** //
+    it("Set Token Price WRONG",async function(){
+        await GenerateMarkets(false);
+        let tokenId = 0;
+        let tokenPrice = 10;
+        await Market_1.methods.mintToken(tokenId, user_1, tokenPrice).send({from: issuer_1, gas: Gas, value: mintingFee}, function(error, result){});
+        try{
+            await Market_1.methods.setTokenPrice(tokenId, 2 * tokenPrice).send({from: issuer_1, gas: Gas}, function(error, result){});
+            expect.fail();
+        }
+        // assert
+        catch(error){
+            expect(error.message).to.match(OnlyOwnerOrApproved);
+        }
+      
+    });
+
+    it("Set Token Price CORRECT",async function(){
+        await GenerateMarkets(false);
+        let tokenId = 0;
+        let tokenPrice = 10;
+        await Market_1.methods.mintToken(tokenId, user_1, tokenPrice).send({from: issuer_1, gas: Gas, value: mintingFee}, function(error, result){});
+        await Market_1.methods.setTokenPrice(tokenId, 2 * tokenPrice).send({from: user_1, gas: Gas}, function(error, result){});
+        let token = await Market_1.methods.retrieveToken(tokenId).call();
+
+        expect(token[0]._price).to.equal((2 * tokenPrice).toString());
+    });
 
 });
 
